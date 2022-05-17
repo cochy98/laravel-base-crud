20872a967c3a72792aa573af4d618be34bf03847
cochy98 <cosmoferrigno@gmail.com>
2022-05-17 15:04:36 +0200

    Created scaffolding

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,11 @@
+{{-- Implemento la pagina main --}}
+@extends('layouts.main')
+
+{{-- Do un titolo a questa section --}}
+@section('title', 'index')
+
+{{-- Punto dove implemento questa section nella pagina principale --}}
+@section('main')    
+
+
+@endsection
