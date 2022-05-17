c3785c0b7353c894c94f348de114e486bb505baa
cochy98 <cosmoferrigno@gmail.com>
2022-05-17 15:49:41 +0200

    Add faker seeder

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Comic;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -9,8 +11,19 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 80; $i++) {
+            $newComic = new Comic();
+            $newComic->title = $faker->sentence(3);
+            $newComic->description = $faker->paragraph(6);
+            $random = rand(1000, 1083);
+            $newComic->thumb = 'https://picsum.photos/id/' . $random . '/200/300';
+            $newComic->price = $faker->randomFloat(2, 5, 100);
+            $newComic->series = $faker->sentence(2);
+            $newComic->sale_date = $faker->date();
+            $newComic->type = $faker->sentence(2);
+            $newComic->save();
+        }
     }
 }
