227547f951e3096136735d03deb8a6c7b443ba65
cochy98 <cosmoferrigno@gmail.com>
2022-05-21 19:15:48 +0200

    Update style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -13,29 +13,34 @@
       </div>
     </div>
   @endif
-  <div class="my-maxy-card">
-    <h1>{{ $comic->title }}</h1>
-    <div class="box-card-img">
-      <img src="{{ $comic->thumb }}" class="maxy-card-img" alt="Picture of {{ $comic->title }}">
-      <a href="{{ route("comics.edit", $comic ) }}" class="btn btn-warning">Modifica</a>
-      <form action="{{ route('comics.destroy', $comic) }}" method="post" class="delete">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-danger">Elimina</button>
-      </form>
-    </div>
-    <div class="d-flex justify-space-betwenn">
-      <div>
-        <p><strong>Serie: </strong> {{ $comic->series }}</p>
-        <p><strong>Tipo: </strong> {{ $comic->type }}</p>
+  <div class="row align-items-end">
+    <div class="col-6">
+      <div class="my-maxy-card">
+        <h1>{{ $comic->title }}</h1>
+        <div class="box-card-img">
+          <img src="{{ $comic->thumb }}" class="maxy-card-img" alt="Picture of {{ $comic->title }}">
+          <a href="{{ route("comics.edit", $comic ) }}" class="btn btn-warning">Modifica</a>
+          <form action="{{ route('comics.destroy', $comic) }}" method="post" class="delete">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger">Elimina</button>
+          </form>
+        </div>
+        <div class="d-flex justify-content-between">
+          <div>
+            <p class="m-0"><strong>Serie: </strong> {{ $comic->series }}</p>
+            <p class="m-0"><strong>Tipo: </strong> {{ $comic->type }}</p>
+          </div>
+          <div>
+            <small>{{ $comic->sale_date }}</small>
+            -- <span>{{ $comic->price }}€</span>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <small>{{ $comic->sale_date }}</small>
-        -- <span>{{ $comic->price }}€</span>
-      </div>
     </div>
-    <p class="my-card-desc">{{ $comic->description }}</p>
+    <div class="col-6">
+      <p class="my-card-desc">{{ $comic->description }}</p>
+    </div>
   </div>
 </div>
     
