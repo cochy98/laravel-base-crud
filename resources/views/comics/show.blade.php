5f8868b125366600eb5e213a9ec44472576165b9
cochy98 <cosmoferrigno@gmail.com>
2022-05-21 18:41:52 +0200

    Add destroy on show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,7 +6,15 @@
 <div class="container my-5">
   <div class="my-maxy-card">
     <h1>{{ $comic->title }}</h1>
-    <img src="{{ $comic->thumb }}" class="maxy-card-img" alt="Picture of {{ $comic->title }}">
+    <div class="box-card-img">
+      <img src="{{ $comic->thumb }}" class="maxy-card-img" alt="Picture of {{ $comic->title }}">
+      <button class="btn btn-warning">Modifica</button>
+      <form action="{{ route('comics.destroy', $comic) }}" method="post" class="delete">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-danger">Elimina</button>
+      </form>
+    </div>
     <div class="d-flex justify-space-betwenn">
       <div>
         <p><strong>Serie: </strong> {{ $comic->series }}</p>
